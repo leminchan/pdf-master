21d2736d2cc4109921265b28d452a4be833a41c4
Mit Chan <leminc@uci.edu>
2019-08-20 15:14:39 +0700

    Made merge functions its own python module and store inside dep folder

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# pdf-master
+# PDFmaster
+PDFmaster is a Python script to merge PDF files together.
+
+## Description
+This was done as a personal project to avoid using third-party PDF editors such as PDFmerge. I made the script usage similar to command line arguments to allow for multiple actions with one script.
+
+## Installation
+Install the required dependencies, PyPDF2 and argparse.
+```bash
+pip install PyPDF2
+pip install argparse
+```
+
+## Usage
+usage: 
+```bash
+py masterpdf.py [-h] [-f [F [F ...]]] [-d D]
+```
+
+If you want to merge multiple files in consecutive order, use the -f command as so:
+```bash
+py masterpdf.py -f pdf_one.pdf pdf_two.pdf ...
+```
+
+If you would like to merge all pdf's in a directory, use the -d command as so:
+```bash
+py masterpdf.py -d directory_containing_pdf
+```
+
+Users can chain / use multiple commands in one call!
+
+## Project Status
+In the process of adding functions like splitting a PDF, exporting text and images from a PDF, and merging with specified page numbers for PDFmaster.
--- masterpdf.py
+++ masterpdf.py
@@ -1,46 +1,6 @@
-from pathlib import Path
-import PyPDF2
-import sys, argparse, os.path
+import sys, argparse
 
-def checkValidFiles(files: [str]) -> bool:
-    '''
-    Function to check if all Paths in a 
-    given list are paths to actual files.
-    '''
-    # Turns all strings in the list into Paths
-    paths = [Path(f) for f in files]
-    for p in paths:
-        if p.is_file() is not True: 
-            print("Merge failed:\n" + str(p) + " is not a valid PDF.")
-            return False
-    return True
-
-def getFiles(directory: [str]) -> [str]:
-    '''
-    Function to return a list of files in a specified directory.
-    This function also checks if the given directory is also a valid one.
-    '''
-    files = []
-    path_to_directory = Path(directory[0])
-    if path_to_directory.is_dir() is not True:
-        print("Merge failed:]n" + directory[0] + " is not a valid directory.")
-        sys.exit(2)
-    files = [directory[0]+f for f in os.listdir(path_to_directory) if f.endswith('.pdf')]
-    return files
-
-def mergeFiles(files: [str]) -> None:
-    '''
-    First checks if all files are valid.
-    Then, creates the new PDF object, finally writing to 
-    "output.pdf" after all files have been appended.
-    '''
-    if checkValidFiles(files):
-        newPDF = PyPDF2.PdfFileMerger()
-        for f in files:
-            newPDF.append(f)
-        with open("output.pdf", 'wb') as outputfile:
-            newPDF.write(outputfile)
-            newPDF.close()
+from dep import merge
 
 if __name__ == "__main__":
     commands = dict()
@@ -51,13 +11,14 @@ if __name__ == "__main__":
         args = parser.parse_args()
         commands = vars(args)
     except:
+        print("Error occurred.")
         sys.exit(2)
 
     # args.m is a list of strings containing the given arguments.
     if args.f:
-        mergeFiles(args.f)
+        merge.mergeFiles(args.f)
 
     # args.f is list of one string specifying the directory.
     if args.d:
-        files = getFiles(args.d)
-        mergeFiles(files)
+        files = merge.getFiles(args.d)
+        merge.mergeFiles(files)
